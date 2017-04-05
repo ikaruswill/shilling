19a28086da6d20a28ad1c2dd0592e7bf5afb13a8
kangsoontan <tankangsoon@gmail.com>
2017-04-05 22:59:05 +0800

    chart 3

--- static/chart3.js
+++ static/chart3.js
@@ -1,45 +1,18 @@
-var ctx = document.getElementById("chart3Vis").getContext("2d");
+var title = "Past day's expenditure";
+var earliestTime = getEarliestTime(option);
 
-ctx.canvas.width = 300;
-ctx.canvas.height = 300;
+if (option === "lastWeek") {
+    title = "Past week's expenditure";
+} else if (option === "lastMonth") {
+    title = "Past month's expenditure"
+}
 
-var myBarChart = new Chart(ctx, {
-    type: 'horizontalBar',
-    data: {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
-        datasets: [
-            {
-                label: "My First dataset",
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1,
-                data: [65, 59, 80, 81, 56, 55, 40],
-            }
-        ]
-    },
-    options: {
-        scales: {
-            xAxes: [{
-                stacked: true
-            }],
-            yAxes: [{
-                stacked: true
-            }]
-        },
-        responsive: false
+$("#table tbody tr").remove();
+
+_.each(data, function(transaction, index, transactions) {
+    if (transaction.date >= earliestTime) {
+        $("#table tbody").append('<tr><td>' + transaction.amount * -1 +
+                                 '</td><td>' + transaction.category_id +
+                                 '</td><td>' + transaction.item + '</td></tr>');
     }
 });
