beffdee6359265891e5c5c42b9ae09f43d7fbe52
Will Ho <ikaruswill.ho@gmail.com>
2017-04-09 11:53:46 +0800

    Add pid, daemonize and touch-reload to uwsgi run script

--- run_server.sh
+++ run_server.sh
@@ -1,1 +1,1 @@
-uwsgi -s /tmp/shilling.ikaruswill.com.sock --manage-script-name --mount /=app:app  --virtualenv /home/shilling/venv/ --chmod-socket=666
+uwsgi -s /tmp/shilling.ikaruswill.com.sock --manage-script-name --mount /=app:app  -H /home/shilling/env/ --chmod-socket=666 --daemonize /tmp/shilling.log --pidfile /tmp/shilling.pid --touch-reload /home/shilling/uwsgi-reload
