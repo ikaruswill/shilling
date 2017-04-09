e2b046cd68a0fa03c8a6b8c3008dd0ca3542e375
Will Ho <ikaruswill.ho@gmail.com>
2017-04-09 14:21:14 +0800

    Fix start.sh starting server in the home directory

--- uwsgi_control/start.sh
+++ uwsgi_control/start.sh
@@ -1,1 +1,2 @@
+cd ~/www/
 uwsgi -s /tmp/shilling.ikaruswill.com.sock --manage-script-name --mount /=app:app  -H /home/shilling/env/ --chmod-socket=666 --daemonize /tmp/shilling.log --pidfile /tmp/shilling.pid --touch-reload /home/shilling/uwsgi-reload
